a19eaed24964c8f8f6fe3126d1e41a9a2fced997
Charita Sharma <charitasharma1901@gmail.com>
2025-04-16 10:30:49 +0530

    java streams

---
+++ SeleniumUdemy/src/section14/test1.java
@@ -0,0 +1,94 @@
+package section14;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+
+	// Count the number of names starting with alphabet 'A' in list
+
+	@Test(enabled = false)
+	public void regular() {
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Abhira");
+		names.add("Jatin");
+		names.add("Palak");
+		names.add("Anshika");
+		names.add("Anaconda");
+
+		int count = 0;
+		for (int i = 0; i < names.size(); i++) {
+			String actual = names.get(i);
+			if (actual.startsWith("A")) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+	@Test (enabled = false)
+	public void streamFilter() {
+
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("Abhira");
+		names.add("Jatin");
+		names.add("Palak");
+		names.add("An");
+		names.add("Anaconda");
+
+		// there is no life of intermediate operator if there is no terminal op
+		// terminal op will execute only if intermediate op (filter) returns true
+		// We can create stream
+		// how to use filter in stream api
+		long counts = names.stream().filter(s -> s.startsWith("A")).count();
+		System.out.println(counts);
+
+		// print all names of ArrayList
+		names.stream().filter(s -> s.length() > 4).forEach(s -> System.out.println(s));
+		names.stream().filter(s -> s.length() > 4).limit(1).forEach(s -> System.out.println(s));
+		names.stream().filter(s -> s.length() > 4).filter(s -> s.startsWith("A")).limit(1)
+				.forEach(s -> System.out.println(s));
+
+		long d = Stream.of("Abhira", "Jatin", "Palak", "Anshika", "Anaconda").filter(s -> {
+			s.startsWith("A"); // evaluates to true or false
+			return false; // reject every element in stream, regardless of its value ; stream is empty
+							// hence count returned is 0.
+		}).count();
+		System.out.println(d);
+
+		Stream.of("Abhira", "Jatin", "Palak", "Anny", "Anaconda")
+				.filter(s -> s.startsWith("A"))
+				.limit(1)
+				.forEach(s -> System.out.println("Selected : " + s));
+	}
+
+	@Test
+	public void streamMap() {
+		//print names that ends with 'a' with upper case
+		
+		//Manipulate the stream - map
+		Stream.of("Abhira", "Jatin", "Palak", "Anny", "Anaconda")
+		.filter(s -> s.endsWith("a"))
+		.map(s -> s.toUpperCase())
+		.forEach(s -> System.out.println("Name in Uppercase : " + s));
+		
+		//print names which have first letter as A , Uppercase and Sorted
+		Stream.of("Anny","Jatin","Palak","Anaconda","Abhira").filter(s->s.startsWith("A")).map(s->s.toUpperCase()).sorted().forEach(s->System.out.println(s));
+		
+		//Merge two lists
+		List<String> fruitnames =Arrays.asList("Avocado","Orange","Grapes","Banana","Apple","Tomato");
+		
+		ArrayList<String> veggienames = new ArrayList<String>();
+		veggienames.add("Potato");
+		veggienames.add("Onion");
+		veggienames.add("Carrot");
+		veggienames.add("Tomato");
+		
+		
+	}
+	
+}
--- learningJava/src/programs/Anagram.java
+++ learningJava/src/programs/Anagram.java
@@ -3,8 +3,9 @@ package programs;
 public class Anagram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 }
+	
