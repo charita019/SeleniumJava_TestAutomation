b7df70f2e17667a9dfe28fcb2a7750ad402470bf
Charita Sharma <charitasharma1901@gmail.com>
2025-05-08 00:00:10 +0530

    missingnumberprogram

---
+++ learningJava/src/programs/MissingNumberInArray.java
@@ -0,0 +1,21 @@
+package programs;
+
+public class MissingNumberInArray {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 3, 2, 6, 4, 7 };
+
+		findmissingnumber(arr);
+
+	}
+
+	public static void findmissingnumber(int[] arr) {
+		boolean isfound = false;
+
+		for (int i = 0; i < arr.length; i++) {
+			
+		}
+	}
+
+}
